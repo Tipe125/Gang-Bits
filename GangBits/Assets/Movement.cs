8fc1d0a4f4888aa945d6e51eb161f3f705fbac90
Dominic Hawkins <dominic2.hawkins@live.uwe.ac.uk>
2017-09-27 14:02:29 +0100

    Arm movement
    Added arm movement and basic movement script

---
+++ GangBits/Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 15.0f;
+       // var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, 0, 0);
+        //transform.Translate(0, 0, z);
+    }
+}
---
+++ GangBits/Assets/Punch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour {
+    // Use this for initialization
+    public Transform secondpoint;
+    //private bool punch = false;
+    private Vector3 position1;
+	void Start () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
+            position1 = transform.position;
+
+            var x = Time.deltaTime * 150.0f;
+           // var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            
+            //transform.Translate(0, 0, z);
+
+
+        if (Input.GetButton("e")) //Rotation Clockwise
+        {
+            transform.RotateAround(transform.parent.position, Vector3.back, 180 * Time.deltaTime);
+        }
+
+        if (Input.GetButton("q")) //Rotation Counterclockwise
+        {
+            transform.RotateAround(transform.parent.position, Vector3.back, -180 * Time.deltaTime);
+        }
+        var punchX = Time.deltaTime * 3.0f;
+        if (Input.GetButton("w")) //Punch
+        {
+           // if (punch == false)
+           // {
+                //print("false");
+               
+                position1.x = Mathf.Lerp(transform.position.x, secondpoint.position.x, Time.deltaTime * 3.0f);
+
+                transform.position = position1;
+               // punch = true;
+          //  }
+            //else if (punch == true)
+           // {
+               // print("true");
+                
+                position1.x = Mathf.Lerp(secondpoint.position.x, transform.parent.position.x, Time.deltaTime * 3.0f);
+
+                transform.position = position1;
+              //  punch = false;
+ 
+                
+           // }
+
+
+        }
+    }
+}
