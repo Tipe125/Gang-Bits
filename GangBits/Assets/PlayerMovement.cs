502506ba518bfdd3585b32ea76d319490af73732
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-09-28 11:55:42 +0100

    Working Jump
    -working jump
    -**controls feel a lot nicer using gamepad**

--- GangBits/Assets/PlayerMovement.cs
+++ GangBits/Assets/PlayerMovement.cs
@@ -6,61 +6,71 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rig;
 
-    public float speed = 0.5f;
+    public float speed = 5f;
     public float max_force = 20f;
     public Vector2 moveDirection = Vector2.zero;
-    public float jump_height = 2.0f;
+    public float jump_height = 1.0f;
+    public float gravity = 50.0f;
 
     private Vector2 impulse_force = Vector2.zero;
     private float dist_to_ground = 1.0f;
-    private float gravity = 9.8f;
 
 
     /*.................................................
       IMPULSE BASED X,Y MOVEMENT
-      FOR RIGIDBODY
-      NO JUMP AS OF YET (well there is, but it's pretty broken)
+      (works much better using gamepad)
       .................................................
     */
 
 
     void Start()
     {
+        //Initialisations
         rig = GetComponent<Rigidbody>();
-        rig.freezeRotation = true;
+        rig.mass = 5.0f;
         rig.drag = 5.0f;
+        rig.freezeRotation = true;
+        rig.useGravity = false;
     }
 
 
     //Update is called once per frame
     void FixedUpdate()
     {
-
         //gain move direction from x axis
         moveDirection.x = (Input.GetAxis("Horizontal"));
-
         //create impulse force and clamp magnitude to somethign reasonable
         impulse_force = Vector2.ClampMagnitude((speed * moveDirection * rig.mass), max_force);
 
         if (isGrounded())
         {
             if(Input.GetButton("Jump"))
-            {
-                //un-comment this to try to fix the jump
-                //moveDirection.y = JumpValue();
+            {              
+                moveDirection.y = JumpValue();
             }
-
             //add force as impulse to the rigidbody
-            rig.AddForce(impulse_force, ForceMode.Impulse);
+        }
+        else
+        {
+            //reset y movement so we don't continuously jump
+            moveDirection.y = 0;
         }
 
+        rig.AddForce(impulse_force, ForceMode.Impulse);
         rig.AddForce(new Vector2(0, -gravity * rig.mass));
-
     }
+
 
     float JumpValue()
     {
-        return Mathf.Sqrt(2 * jump_height * gravity);
+        if(isGrounded())
+        {
+            return Mathf.Sqrt(2 * jump_height * gravity);
+        }
+        else
+        {
+            return 0;
+        }
     }
 
 
