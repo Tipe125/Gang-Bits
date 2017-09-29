b5ac8aedbb314c668a87d2596726fc76c8c261e4
Mythalore <samfrichards@gmail.com>
2017-09-29 13:23:34 +0100

    Started on animations for truck level

---
+++ GangBits/Assets/Truck_Trucks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truck_Trucks : MonoBehaviour {
+
+	bool isMoving;
+	bool isForward;
+	float moveDelay;
+	float moveCounter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moveCounter >= moveDelay){
+			isMoving = true;
+			
+		}
+	}
+}
